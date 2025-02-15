8a95ac96d45a0aebb6e2a4c79bef5b2d8766ce6a
obana ryota <obanaryota0907@gmail.com>
2025-02-15 21:52:35 +0900

    feat:お問合せフォームのデータ受け取り

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -12,13 +12,22 @@ class ContactController extends Controller
     public function index()
     {
       $categories = Category::all();
-      
+
       return view('index', compact('categories'));
     }
 
-    public function confirm()
+    public function confirm(Request $request)
     {
-      return view('confirm');
+      $contact = $request->all();
+
+      // 番号の作り直し
+      $tel = $contact['tel-1'].$contact['tel-2'].$contact['tel-3'];
+
+      // 作り直した番号とリクエストデータでデータ保存へ
+      unset($contact['tel-1'], $contact['tel-2'], $contact['tel-3']);
+      $fixedcontact = $contact + array('tel'=> $tel);
+
+      return view('confirm', ['contact' => $fixedcontact]);
     }
 
     public function store()
