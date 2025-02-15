968e7457877578243afaf6145476cbffb1f336eb
obana ryota <obanaryota0907@gmail.com>
2025-02-15 09:07:13 +0900

    feat:お問い合わせルーティングの設定

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -7,4 +7,18 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     //
+    public function index()
+    {
+      return view('index');
+    }
+
+    public function confirm()
+    {
+      return view('confirm');
+    }
+
+    public function store()
+    {
+      return view('thanks');
+    }
 }
