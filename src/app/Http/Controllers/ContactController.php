0ff971487939bc4c82ee5996f7f241f21af26ffc
obana ryota <obanaryota0907@gmail.com>
2025-02-16 18:01:51 +0900

    feat:確認画面への値の送信

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -22,18 +22,19 @@ class ContactController extends Controller
     public function confirm(ContactRequest $request)
     {
       $contact = $request->all();
+	  $category = Category::find($request->category_id);
 
       // 番号の作り直し
       $tel = $contact['tel-1'].$contact['tel-2'].$contact['tel-3'];
 
-      // 作り直した番号とリクエストデータでデータ保存へ
+      // 作り直した番号とリクエストデータをまとめ、保存できる形に
       unset($contact['tel-1'], $contact['tel-2'], $contact['tel-3']);
       $fixedcontact = $contact + array('tel'=> $tel);
 
-      return view('confirm', ['contact' => $fixedcontact]);
+      return view('confirm', ['contact' => $fixedcontact, 'category' => $category]);
     }
 
-    public function store()
+    public function store(Request $request)
     {
       return view('thanks');
     }
--- src/resources/views/confirm.blade.php
+++ src/resources/views/confirm.blade.php
@@ -21,35 +21,71 @@
 			<table class="confirm-table inika-regular">
 				<tr class="confirm-table__row">
 					<th class="confirm-table__header">お名前</th>
-					<td class="confirm-table__text">サンプルテキスト</td>
+					<td class="confirm-table__text">
+						{{ $contact['last_name']."　".$contact['first_name']}}
+						<input type="text" name="last_name" value="{{ $contact['last_name'] }}" hidden/>
+						<input type="text" name="first_name" value="{{ $contact['first_name'] }}" hidden/>
+					</td>
 				</tr>
 				<tr class="confirm-table__row">
 					<th class="confirm-table__header">性別</th>
-					<td class="confirm-table__text">サンプルテキスト</td>
+					<td class="confirm-table__text">
+					@switch( $contact['gender'] )
+							@case(1)
+								男性
+								@break
+							@case(2)
+								女性
+								@break
+							@case(3)
+								その他
+								@break
+							@default
+						@endswitch
+					<input type="radio" name="gender" value="{{ $contact['gender'] }}" checked hidden/>
+					</td>
 				</tr>
 				<tr class="confirm-table__row">
 					<th class="confirm-table__header">メールアドレス</th>
-					<td class="confirm-table__text">サンプルテキスト</td>
+					<td class="confirm-table__text">
+						{{ $contact['email'] }}
+						<input type="text" name="email" value="{{ $contact['email'] }}" hidden/>
+					</td>
 				</tr>
 				<tr class="confirm-table__row">
 					<th class="confirm-table__header">電話番号</th>
-					<td class="confirm-table__text">サンプルテキスト</td>
+					<td class="confirm-table__text">
+						{{ $contact['tel'] }}
+						<input type="text" name="tel" value="{{ $contact['tel'] }}" hidden/>
+					</td>
 				</tr>
 				<tr class="confirm-table__row">
 					<th class="confirm-table__header">住所</th>
-					<td class="confirm-table__text">サンプルテキスト</td>
+					<td class="confirm-table__text">
+						{{ $contact['address'] }}
+						<input type="text" name="address" value="{{ $contact['address'] }}" hidden/>
+					</td>
 				</tr>
 				<tr class="confirm-table__row">
 					<th class="confirm-table__header">建物名</th>
-					<td class="confirm-table__text">サンプルテキスト</td>
+					<td class="confirm-table__text">
+						{{ $contact['building'] }}
+					<input type="text" name="building" value="{{ $contact['building'] }}" hidden/>
+					</td>
 				</tr>
 				<tr class="confirm-table__row">
 					<th class="confirm-table__header">お問い合わせの種類</th>
-					<td class="confirm-table__text">サンプルテキスト</td>
+					<td class="confirm-table__text">
+						{{ $category['content'] }}
+						<input type="text" name="category_id" value="{{ $contact['category_id'] }}" hidden/>
+					</td>
 				</tr>
 				<tr class="confirm-table__row">
 					<th class="confirm-table__header">お問い合わせの内容</th>
-					<td class="confirm-table__text">サンプルテキスト</td>
+					<td class="confirm-table__text">
+						{{ $contact['detail'] }}
+						<input type="text" name="detail" value="{{ $contact['detail'] }}" hidden/>
+					</td>
 				</tr>
 			</table>
 			<div class="form__button">
