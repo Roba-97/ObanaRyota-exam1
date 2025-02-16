0bc5ac959f3be0c498bb6791420fc84af1aa97a0
obana ryota <obanaryota0907@gmail.com>
2025-02-16 23:17:45 +0900

    feat:ログインページのデザイン作成

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -9,5 +9,35 @@ register
 @endsection
 
 @section('content')
+<div class="content-wrapper">
+	<div class="about">
+		<div class="about__inner">
+			<h2 class="about__text inika-regular">
+				login
+			</h2>
+		</div>
+	</div>
 
+	<div class="login-form">
+		<div class="login-form__inner">
+			<form class="" action="/login" method="post">
+				<div class="form__group">
+					<div class="form__group-title">メールアドレス</div>
+					<div class="form__group-input">
+						<input type="text" name="email" value="{{ old('email') }}" placeholder="例:test@example.com"/>
+					</div>
+				</div>
+				<div class="form__group">
+					<div class="form__group-title">パスワード</div>
+					<div class="form__group-input">
+						<input type="password" name="password" placeholder="例:coachtech1106"/>
+					</div>
+				</div>
+				<div class="form__button">
+					<button class="form__button-submit" type="submit">ログイン</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
 @endsection
